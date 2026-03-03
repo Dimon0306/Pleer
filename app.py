323e228d3f3d1f81cb1d0e69b3cb292dc973f141
Dimon0306 <pisarevskiy.da@bk.ru>
2026-03-03 23:18:46 +0400

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,36 @@ TRACKS = [
     {"id": 10, "title": "Голова", "artist": "Канги",
      "url": "https://moosic.my.mail.ru/file/603c65915f9962684f0476f6222aac34.mp3",
      "duration": "2:55"},
+    {"id": 11, "title": "Не мешай", "artist": "RSAC",
+     "url": "https://moosic.my.mail.ru/file/4d56cf120e06dfd9b09c5e86acacf7b6.mp3",
+     "duration": "2:11"},
+    {"id": 12, "title": "Прыгну со сколы", "artist": "Король и шут",
+     "url": "https://moosic.my.mail.ru/file/3703247dbcec27869a477bc010cf564a.mp3",
+     "duration": "3:10"},
+    {"id": 13, "title": "Ели мясо мужики", "artist": "Король и шут",
+     "url": "https://moosic.my.mail.ru/file/d1b4dbd7925c2d33fc9a923571d3c199.mp3",
+     "duration": "2:55"},
+    {"id": 14, "title": "Лесник", "artist": "Король и шут",
+     "url": "https://moosic.my.mail.ru/file/f4b7315362aadd5b1584c07c9abd20c9.mp3",
+     "duration": "2:50"},
+    {"id": 15, "title": "Ворона", "artist": "Линда",
+     "url": "https://moosic.my.mail.ru/file/27481114089fdcd8bd9522afd3015b2c.mp3",
+     "duration": "3:46"},
+    {"id": 16, "title": "Мой сон", "artist": "Буланова",
+     "url": "https://moosic.my.mail.ru/file/ff931f4b69267943ba1d09fbae33b268.mp3",
+     "duration": "4:30"},
+    {"id": 17, "title": "Не плач", "artist": "Буланова",
+     "url": "https://moosic.my.mail.ru/file/ab3a6d546dad22a354b5c85a3ec1a40c.mp3",
+     "duration": "2:55"},
+    {"id": 18, "title": "На заре", "artist": "Баста",
+     "url": "https://moosic.my.mail.ru/file/26014f2ee17dc762a1dff2066032b083.mp3",
+     "duration": "5:06"},
+    {"id": 19, "title": "Я больше не напишу", "artist": "Тима Белорусских",
+     "url": "https://moosic.my.mail.ru/file/14230aa4795bb4e8f9d4dc18b15eb04c.mp3",
+     "duration": "3:11"},
+    {"id": 20, "title": "Ночная Травма", "artist": "Хан Замай & Слава КПСС",
+     "url": "https://moosic.my.mail.ru/file/fe6b406f03cfa54f9460c643d9272554.mp3",
+     "duration": "3:11"}
 ]
 
 @app.route('/')
@@ -67,6 +97,7 @@ def serve_sw():
 if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 
 
